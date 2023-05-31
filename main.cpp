4cdc09309f1a9593b960e290e3277fa6c9c80226
JOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOHN <135120894+joooooooooohn@users.noreply.github.com>
2023-05-31 13:51:45 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+          fn main() {
+       // bypass x4
+       let ref mut i = 0; *i = "joooooooooohn".matches("o").count() as _;
+       while { *i -= 1; *i > 0 } {
+    if i > &mut 8 || (7 == --*i) | (&i == &&5) || (*i == 6 - *i) | (*i < 2) {
+  print!("by")
+    }
+    else {
+  println!("pass")
+    }
+       }
+          }
